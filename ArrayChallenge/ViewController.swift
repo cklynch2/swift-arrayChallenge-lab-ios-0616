2c3c0d4f5c5372c6d8ca3a8ef53d135ecc6f5062
cklynch2 <cklynch2@gmail.com>
2016-07-07 14:43:39 -0400

    Shopping list fixed to show items alongside quantity

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -11,15 +11,26 @@ import UIKit
 class ViewController: UIViewController {
     
     var shoppingList: [String] = []
+//    var items: [String] = []
+//    var quantityOfItems: [Int] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
+    let items = ["Bananas", "Apples", "Eggs", "Rolls"]
+    let quantityOfItems = [6, 4, 12, 4]
         
+    shoppingList = makeShoppingList(items, quantityOfItems: quantityOfItems)
         
     }
     
     
-    
+    func makeShoppingList(items: [String], quantityOfItems: [Int]) -> [String] {
+        var shoppingList: [String] = []
+        for (index, quantity) in quantityOfItems.enumerate() {
+            let itemWithQuantity = "\(index + 1). \(quantity)" + " \(items[index])"
+            shoppingList.append(itemWithQuantity)
+        }
+        return shoppingList
+    }
 }
